7429c8b4d2935618e5d685adf9f99dc0b89830b7
aymaxcel <cyubahiroaxcel@gmail.com>
2025-05-07 19:56:39 +0200

    added index file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,14 +4,14 @@ function About() {
       <h1>About TechTrends Ltd</h1>
       <p>We specialize in top-quality electronic gadgets and are located in Kigali City.</p>
       <iframe
-        title="TechTrends Location"
-        src="https://www.google.com/maps/embed/v1/place?q=Kigali+City,+Rwanda&key=YOUR_GOOGLE_MAPS_API_KEY"
-        width="100%"
-        height="450"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-      ></iframe>
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.3907873743664!2d30.060506714754175!3d-1.944072998593617!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6e1a2a88aab%3A0x1fabc3f3cb5c5a6b!2sKigali%2C%20Rwanda!5e0!3m2!1sen!2srw!4v1589103198996!5m2!1sen!2srw"
+  width="100%"
+  height="400"
+  style={{ border: 0 }}
+  allowFullScreen=""
+  loading="lazy"
+></iframe>
+
     </div>
   );
 }
